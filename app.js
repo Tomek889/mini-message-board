3d762063931c8ef4463f849e03bdfd67a3c4430c
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-06-25 13:39:24 +0200

    Show the messages from the messages array

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const newRouter = require("./routes/newRouter");
 const indexRouter = require("./routes/indexRouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/new", newRouter);
 app.use("/", indexRouter);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,48 @@
 const { Router } = require("express");
-
 const indexRouter = Router();
+const path = require("node:path");
 
-indexRouter.get("/", (req, res) => res.send("home"));
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+  {
+    text: "How's it going?",
+    user: "Tina",
+    added: new Date(),
+  },
+  {
+    text: "Anyone up for coffee?",
+    user: "Raj",
+    added: new Date(),
+  },
+  {
+    text: "Just finished my homework!",
+    user: "Lena",
+    added: new Date(),
+  },
+  {
+    text: "Node.js is awesome 😎",
+    user: "Carlos",
+    added: new Date(),
+  },
+  {
+    text: "Can't wait for the weekend!",
+    user: "Zoe",
+    added: new Date(),
+  },
+];
+
+indexRouter.get("/", (req, res) => {
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
 
 indexRouter.get("/{*splat}", (req, res) => {
   res.send("Not found (404 error).");
